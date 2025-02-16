f9298f05e09222f816c85fabf22cda71042bed9b
S-Achyutan <afs6531@psu.edu>
2025-02-16 15:44:37 -0500

    added kill switch

--- motor_spinning-main/motor_spinning/src/main.cpp
+++ motor_spinning-main/motor_spinning/src/main.cpp
@@ -1,9 +1,9 @@
 /*
  Motors/ESCs Spinning Direction:
- - Front Right -> Pin 3, CCW
- - Back Right -> Pin 5, CW
- - Back Left -> Pin 9, CCW
- - Front Left -> Pin 10, CW
+ - Front Right -> Pin 3, CCW motor 1
+ - Back Right -> Pin 5, CW motor 2
+ - Back Left -> Pin 9, CCW motor 3
+ - Front Left -> Pin 10, CW motor 4
 */
 #include "wifi.h"
 #include "rc_pilot.h"
@@ -59,43 +59,62 @@ void loop() {
     float dt = (currentMillis - previousMillis) / 1000.0f; // Convert to seconds
     
     rc.update();
-    
-    if(rc.rc_in.AUX > 1500) {
-        // Manual RC control mode
-        float throttle = manualMap(rc.rc_in.THR, 1000, 2000, -1.0, 1.0);
-        float roll = manualMap(rc.rc_in.ROLL, 1000, 2000, -1.0, 1.0);
-        float pitch = manualMap(rc.rc_in.PITCH, 1000, 2000, -1.0, 1.0);
-        float yaw = manualMap(rc.rc_in.YAW, 1000, 2000, -1.0, 1.0);
+    if (rc.rc_in.AUX < 1500){
+        if(rc.rc_in.AUX2 > 1200) {
+            // Manual RC control mode
+            float throttle = manualMap(rc.rc_in.THR, 1000, 2000, -1.0, 1.0);
+            float roll = manualMap(rc.rc_in.ROLL, 1000, 2000, -1.0, 1.0);
+            float pitch = manualMap(rc.rc_in.PITCH, 1000, 2000, -1.0, 1.0);
+            float yaw = manualMap(rc.rc_in.YAW, 1000, 2000, -1.0, 1.0);
 
-        // Serial.print(rc.rc_in.THR) ;  Serial.print(", "); 
-        // Serial.print(throttle) ;  Serial.print(", "); 
-        // Serial.print(pitch) ;  Serial.print(", "); 
-        // Serial.print(", \n"); 
-        
-        // Mix directly from RC inputs
-        mixer.mix(throttle, roll, pitch, yaw, pwm);
-    } else {
+            // Serial.print(rc.rc_in.THR) ;  Serial.print(", "); 
+            // Serial.print(throttle) ;  Serial.print(", "); 
+            // Serial.print(pitch) ;  Serial.print(", "); 
+            // Serial.print(", \n"); 
+            
+            // Mix directly from RC inputs
+            mixer.mix(throttle, roll, pitch, yaw, pwm);
+            //Serial.println("MANUAL");
+            if (rc.rc_in.AUX2 > 1700){
+                //Serial.println("Drop Payload") ; 
+            }
+
+        } else {
+            // Autonomous/GCS mode
+            float c_delf = 0, c_delm0 = 0, c_delm1 = 0, c_delm2 = 0;
+            
+            // Update controller with current state
+            //controller.updateControl(dt, 0,  // phiCmd
+            //                       0, 0, 0,  // posDes_x, y, z
+            //                       0, 0, 0,  // nav_p_x, y, z
+            //                       0, 0, 0,  // nav_v_x, y, z
+            //                       0, 0, 0,  // nav_w_x, y, z
+            //                       0, 0, 0,  // nav_phi, theta, psi
+            //                      c_delf, c_delm0, c_delm1, c_delm2);
+            
+            // Mix controller outputs
+            mixer.mix(c_delf, c_delm0, c_delm1, c_delm2, pwm);
+            //Serial.println("AUTO");
+        }  
+    }else{ 
         // Autonomous/GCS mode
-        float c_delf = 0, c_delm0 = 0, c_delm1 = 0, c_delm2 = 0;
+        pwm[0] = MIN_PWM_OUT ;
+        pwm[1] = MIN_PWM_OUT ;
+        pwm[2] = MIN_PWM_OUT ;
+        pwm[3] = MIN_PWM_OUT ; 
         
-        // Update controller with current state
-        controller.updateControl(dt, 0,  // phiCmd
-                               0, 0, 0,  // posDes_x, y, z
-                               0, 0, 0,  // nav_p_x, y, z
-                               0, 0, 0,  // nav_v_x, y, z
-                               0, 0, 0,  // nav_w_x, y, z
-                               0, 0, 0,  // nav_phi, theta, psi
-                               c_delf, c_delm0, c_delm1, c_delm2);
-        
-        // Mix controller outputs
-        mixer.mix(c_delf, c_delm0, c_delm1, c_delm2, pwm);
+        Serial.println("KILLED");
+        Serial.print(pwm[0]) ; Serial.print(",") ; 
+        Serial.print(pwm[1]) ; Serial.print(",") ; 
+        Serial.print(pwm[2]) ; Serial.print(",") ; 
+        Serial.print(pwm[3]) ; 
     }
     
     motors.update(pwm);
     
     if (currentMillis - previousMillis >= interval) {
         previousMillis = currentMillis;
-     //   rc.print(); // should be commented out for flight
+        // rc.print(); // should be commented out for flight
     }
     
     // read/write datalink msg
